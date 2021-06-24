c564df434564befd67fd9a8309030e833a11b7c1
flours <flours@users.noreply.github.com>
2021-06-24 06:45:34 +0000

    Apply Code Formatter Change

--- Python/Binomial_theorem.py
+++ Python/Binomial_theorem.py
@@ -1,22 +1,24 @@
-def genCombinationFunction(max_k,mod):
-    modinv_table = [-1] * (max_k+1)
+def genCombinationFunction(max_k, mod):
+    modinv_table = [-1] * (max_k + 1)
     modinv_table[1] = 1
-    for i in range(2, max_k+1):
+    for i in range(2, max_k + 1):
         modinv_table[i] = (-modinv_table[mod % i] * (mod // i)) % mod
 
     def binomial_coefficients(n, k):
         ans = 1
         for i in range(k):
-            ans *= n-i
+            ans *= n - i
             ans *= modinv_table[i + 1]
             ans %= mod
         return ans
+
     return binomial_coefficients
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     n = 10 ** 9
     k = 2 * 10 ** 5
-    mod = 10**9 + 7
-    f=genCombinationFunction(k,mod)
-    print(f(n,k))
-    print(f(10,3))
+    mod = 10 ** 9 + 7
+    f = genCombinationFunction(k, mod)
+    print(f(n, k))
+    print(f(10, 3))
