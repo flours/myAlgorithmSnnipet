8295142147d9dac7eaef111ff4110c58fa6ad477
flours <flours@users.noreply.github.com>
2022-03-04 13:31:37 +0000

    Apply Code Formatter Change

--- Python/Binomial_theorem.py
+++ Python/Binomial_theorem.py
@@ -13,18 +13,21 @@ def genCombinationFunction(max_k, mod):
         return ans
 
     return binomial_coefficients
+
+
 # 前処理O(N)でO(1)nCr
 
-def genO1CombinationFunction(N,mod):
-    table=[1]
-    inv_table=[1]
 
-    for i in range(1,N):
-      table+=table[-1]*i%mod,
-      inv_table+=pow(table[-1],mod-2,mod),
+def genO1CombinationFunction(N, mod):
+    table = [1]
+    inv_table = [1]
 
-    def nCr(n,r):
-      return table[n]*inv_table[r]*inv_table[n-r]%mod
+    for i in range(1, N):
+        table += (table[-1] * i % mod,)
+        inv_table += (pow(table[-1], mod - 2, mod),)
+
+    def nCr(n, r):
+        return table[n] * inv_table[r] * inv_table[n - r] % mod
 
     return nCr
 
@@ -36,5 +39,5 @@ if __name__ == "__main__":
     f = genCombinationFunction(k, mod)
     print(f(n, k))
     print(f(10, 3))
-    f=genO1CombinationFunction(10**6, 998244353)
-    print(f(10,3))
+    f = genO1CombinationFunction(10 ** 6, 998244353)
+    print(f(10, 3))
